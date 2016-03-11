51445e006aaeec5c75666a2525318c914b3801b4
Sean Purcell <iburinoc@gmail.com>
2016-03-11 12:48:19 -0500

    Factored out display creating and arg handling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 #[macro_use]
 extern crate glium;
+extern crate clap;
+
+use glium::glutin::WindowBuilder;
+use glium::DisplayBuild;
+
+use clap::{Arg, App, ArgMatches};
+
+mod settings;
 
 fn main() {
 	use glium::DisplayBuild;
 
-	let display = glium::glutin::WindowBuilder::new()
-		.with_dimensions(1024, 768)
-		.with_title(format!("Hello world"))
-		.build_glium()
-		.unwrap();
+    let args = arg_handle();
+	let display = build_display().build_glium().unwrap();
+
+    println!("{}", settings::NAME);
 
 	loop {
 		for ev in display.wait_events() {
@@ -18,5 +25,19 @@ fn main() {
 			}
 		}
 	}
+}
+
+fn build_display<'a>() -> WindowBuilder<'a> {
+    WindowBuilder::new()
+		.with_dimensions(1024, 768)
+		.with_title(format!("gr-trace"))
+}
+
+fn arg_handle<'a>() -> ArgMatches<'a> {
+    App::new(settings::NAME)
+        .version(settings::VERSION)
+        .author("Sean Purcell <iburinoc@gmail.com>")
+        .about("GPU General Relativity Ray Tracer")
+        .get_matches()
 }
 
---
+++ src/settings.rs
@@ -0,0 +1,4 @@
+#![allow(dead_code)]
+pub const NAME: &'static str = "gr-trace";
+pub const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
